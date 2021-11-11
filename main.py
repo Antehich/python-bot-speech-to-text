9a25eb6874abcf6a89b1e2204c4784ff23cb4437
Antehich <91225105+Antehich@users.noreply.github.com>
2021-11-11 14:59:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,117 @@
+import logging
+import urllib.request
+import json
+
+FOLDER_ID = "b1g4kr4rssk1hvmadd49"  # Идентификатор каталога
+IAM_TOKEN = "t1.9euelZqcxovLnZmPl8iRm8zOxpzHle3rnpWai8_Oyc6Pm5qYnpvIlIuNm5Dl9PdKfwJ1-e9JRCnN3fT3Ci4AdfnvSUQpzQ.yJ2J-D0X-qieW7xCDKPeB6vAMW5cYCMH5i3jxr4e9xD6rTmbWW18jeEjparP8S61capQVVclJ20vA0pUabHFCw"  # IAM-токен
+
+from telegram import Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+VOICE = int(1)
+
+
+def get_text_from_voice() -> json:
+    with open("user_voice.ogg", "rb") as f:
+        data = f.read()
+
+    params = "&".join([
+        "topic=general",
+        "folderId=%s" % FOLDER_ID,
+        "lang=ru-RU"
+    ])
+
+    url = urllib.request.Request("https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?%s" % params, data=data)
+    url.add_header("Authorization", "Bearer %s" % IAM_TOKEN)
+
+    responseData = urllib.request.urlopen(url).read().decode('UTF-8')
+    decodedData = json.loads(responseData)
+    return decodedData
+
+
+def start(update: Update, context: CallbackContext) -> int:
+    """Starts the conversation and asks the user about their gender."""
+    update.message.reply_text(
+        'Пришлите свое голосовое сообщение',
+    )
+    return VOICE
+
+
+def cancel(update: Update, context: CallbackContext) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text(
+        'Bye! I hope we can talk again some day.'
+    )
+
+    return ConversationHandler.END
+
+
+def voice(update: Update, context: CallbackContext) -> int:
+    """Stores the voice and asks for a location."""
+    user = update.message.from_user
+    voice_file = update.message.voice.get_file()
+    voice_file.download('user_voice.ogg')
+    logger.info("voice of %s: %s", user.first_name, 'user_voice.ogg')
+    update.message.reply_text(
+        'Замечательно! Обрабатываю.'
+    )
+    decodedData = get_text_from_voice()
+    if decodedData.get("error_code") is None:
+        if decodedData.get("result") == '':
+            update.message.reply_text('Прислано пустое сообщение!')
+        else:
+            update.message.reply_text(
+                decodedData.get("result")
+            )
+            print(decodedData.get("result"))
+    else:
+        update.message.reply_text('Произошла ошибка, попробуйте позднее!')
+
+    return VOICE
+
+
+def main() -> None:
+    """Run the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater("2003103737:AAGjcP6B2Eupe8td6hCdWCAY0Mq4V1giUDM")
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # Add conversation handler with the states GENDER, voice, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            VOICE: [MessageHandler(Filters.voice, voice)], },
+        fallbacks=[CommandHandler('cancel', cancel)],
+
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+    # Start the Bot
+    updater.start_polling()
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
